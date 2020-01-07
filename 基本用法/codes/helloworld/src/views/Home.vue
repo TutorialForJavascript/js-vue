47a81721bc64408207a4b88a36bfb1bb832896a1
hsz1273327 <hsz1273327@gmail.com>
2020-01-07 20:00:31 +0800

    update

---
+++ 基本用法/codes/helloworld/src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <form>
+      <fieldset>
+        <legend>send message</legend>
+        <p>
+          message:
+          <input type="text" name="message" v-model="msg" />
+        </p>
+      </fieldset>
+    </form>
+    <HelloWorld :msg="msg" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  data: function() {
+    return {
+      msg: undefined
+    };
+  },
+  watch: {
+    msg: function(val, oldVal) {
+      if ((val === null) | (val === "")) {
+        this.msg = undefined;
+      }
+    }
+  },
+  components: {
+    HelloWorld
+  }
+};
+</script>
